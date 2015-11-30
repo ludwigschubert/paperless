a5c84b979f086a0407a13135bfa7eeaa21f9ef3a
Ludwig Schubert <ludwig.schubert@qlearning.de>
2015-11-29 21:52:07 -0800

    Basic Message structure

--- js/components/Message.jsx
+++ js/components/Message.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Image, Col, Row } from 'react-bootstrap';
 
 let Message = React.createClass({
 
   render() {
+    const message = this.props.message;
+
+    var text;
+    if (message.text) {
+      text = <div className="message-text">{message.text}</div>;
+    }
+
+    var image;
+    if (message.imageUrl) {
+      image = <Row>
+        <Col sm={6}>
+          <Image rounded responsive className="message-image" src={message.imageUrl} />
+        </Col>
+      </Row>
+    }
+
+    const messageClass = message.isReply ? "message-reply" : "message-incoming";
+
     return(
-      <p>
-        THIS IS A MESSAGE
-      </p>
+      <div className={"message " + messageClass}>
+        <div className="message-timestamp">
+          {message.createdAt.toString()}
+        </div>
+        {text}
+        {image}
+      </div>
     );
 
   }
--- js/components/MessagesList.jsx
+++ js/components/MessagesList.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Parse from 'parse';
+import ParseReact from 'parse-react';
+
 import { Col } from 'react-bootstrap';
+import Message from './Message.jsx'
 
 let MessagesList = React.createClass({
+  mixins: [ParseReact.Mixin], // Enable query subscriptions
+
+  observe: function() {
+    return {
+      messages: (new Parse.Query('Message')).equalTo('patron', this.props.patron).ascending('createdAt')
+    };
+  },
 
   render() {
     return(
       <Col sm={6} className="messages-list-container">
-      {this.props.patron.name}s MESSAGES LIST
+        {this.data.messages.map( function(message) {
+          return <Message key={message.objectId} message={message}/>;
+        })}
       </Col>
     );
 
