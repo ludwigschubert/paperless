301b246c7fa8d0c39253e7491eebf369a1559412
Ludwig Schubert <ludwig.schubert@qlearning.de>
2015-12-01 12:51:17 -0800

    Allow Message Sending by pressing return

--- js/components/MessagesCreator.jsx
+++ js/components/MessagesCreator.jsx
@@ -21,41 +21,16 @@ let MessagesCreator = React.createClass({
     }).dispatch();
 
     this.setState({ text: '' });
+    
+    return false;
   },
-
-  // mixins: [ParseReact.Mixin], // Enable query subscriptions
-
-  // observe: function() {
-  //   return {
-  //     messages: (new Parse.Query('Message')).equalTo('patron', this.props.patron).ascending('createdAt')
-  //   };
-  // },
-  //
-  // componentDidMount: function() {
-  //   const pubnub = PUBNUB({
-  //       publish_key   : 'pub-c-a0daec15-afc0-4588-9b0a-e419807f5882',
-  //       subscribe_key : 'sub-c-17e1a790-9737-11e5-b829-02ee2ddab7fe'
-  //   })
-  //   const component = this;
-  //   pubnub.subscribe({
-  //       channel : "new-message",
-  //       message : function(message,env,ch,timer,magic_ch){
-  //         const messageId = JSON.stringify(message);
-  //         console.log("PubNub message received; new message(" + messageId.substr(1, messageId.length -2) + ")"); // gets rid of quotes
-  //         component.refreshQueries('messages');
-  //       },
-  //       connect: function() {
-  //         console.log("...subscribed to PubNub new-message channels.")
-  //       }
-  //   });
-  // },
 
   render() {
     const innerButton = <Button onClick={this._sendMessage} >Send</Button>;
 
     return(
       <Col sm={12} className="messages-creator-container">
-        <form>
+        <form onSubmit={this._sendMessage}>
           <Input
             type="text"
             placeholder={"Message to " + this.props.patron.name }
